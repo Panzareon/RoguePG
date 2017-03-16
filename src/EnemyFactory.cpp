1fece35b60702b3c82bd32dce719181d0c4364a0
Panzareon <Fogtociag@web.de>
2017-03-16 19:03:41 +0100

    Slime balancing

--- src/EnemyFactory.cpp
+++ src/EnemyFactory.cpp
@@ -93,7 +93,7 @@ Entity* EnemyFactory::GetEntity(EnemyList type, int lvl)
         //Exp to give after Battle
         ret = new Entity(10);
         ret->SetName(localization->GetLocalization("enemy.water_slime"));
-        ret->InitAllAttributes(15 + lvl,9 + lvl,3 + lvl/3,9 + lvl,4 + lvl * 2,3 + lvl/3,9 + lvl);
+        ret->InitAllAttributes(15 + lvl,9 + lvl,3 + lvl/3,9 + lvl,12 + lvl,3 + lvl/3,9 + lvl);
         ret->SetBattleSprite(TextureList::WaterSlimeBattleSprite);
         ret->SetResistance(BattleEnums::AttackTypePhysical, 1.5f);
         ret->SetResistance(BattleEnums::AttackTypeEarth, 1.3f);
